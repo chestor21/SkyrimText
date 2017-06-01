26a499d44edf4063a42f9f2a6f672c92728ffc18
chestor21 <chestormaster21@gmail.com>
2017-06-01 14:04:32 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,15 +2,41 @@ class Skyrim
 {
    public static void main (String[] args)
   {
-    // your program here
-   
-   
-   
-   
-   
-    //
-   
-   
+ //Scanner *if needed*
+/*Main Code 
+
+
+Game variables
+Enemies:Monsters
+        Bandits
+        Witchs
+        Boss
+Items:Wepons(Swords +5 Dam ,Bow +3 Dam,Fist +1 Dam)
+      Item:(Health potion +50HP)
+     
+     
+Player variables
+Main character(Name is Cincinnati )
+Harambe (friend that goes missing)
+
+
+
+
+*Play Method
+Displays the play 
+
+*Rules Method
+Displays the rules for the game so player can selcet to view it
+
+*Exist Method
+Exits the menu closes code
+
+
+
+
+
+
+End Code*/
    
    }
 } 
