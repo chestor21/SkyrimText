05636d89e04aed3ceec6a2783e566dde88a1afa4
chestor21 <chestormaster21@gmail.com>
2017-06-02 15:25:16 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
+ //Scanner *if needed*
+
 class Skyrim
 {
    public static void main (String[] args)
   {
- //Scanner *if needed*
 /*Main Code 
+
+Rules+Story:DONT INPUT NUMBERS IT WILL NOT BE NEEDED IN THIS GAME!You have to battle your way through so that you can find your friend Harambe who gone
+missing.Make your choices wisely because just one tiny mistake like forgeting a WEPON or ITEM will get you KILLED!This game is text based.
 
 
 Game variables
@@ -17,13 +21,15 @@ Items:Wepons(Swords +5 Dam ,Bow +3 Dam,Fist +1 Dam)
      
 Player variables
 Main character(Name is Cincinnati )
-Harambe (friend that goes missing)
+Harambe (friend that goes missing/Final Boss)
+JoeBidden(Enemy)
+Sweatpants(Enemy)
 
 
 
 
 *Play Method
-Displays the play 
+Displays the play chocie
 
 *Rules Method
 Displays the rules for the game so player can selcet to view it
@@ -33,7 +39,20 @@ Exits the menu closes code
 
 
 
+Combat method*
+Code for combat in-game 
 
+Map method*
+Code for in-game maps
+
+Enemy methods*
+Code for different enemys that show up(i am gonna make multiple methods of Enemys)
+
+
+Dilouge*
+put this all over the place
+
+Ending of Game*
 
 
 End Code*/
